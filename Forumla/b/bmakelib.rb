8cc48a8c0110c6d6cf5141a197d616d14727b377
Bahman Movaqar <bahman@bahmanm.com>
2023-08-20 04:13:21 -0700

    apply linter suggestions

--- Forumla/b/bmakelib.rb
+++ Forumla/b/bmakelib.rb
@@ -1,31 +1,32 @@
 class Bmakelib < Formula
-  desc "A minimalist standard library for writing Makefiles."
+  desc "bmakelib is a minimalist standard library for writing Makefiles"
   homepage "https://github.com/bahmanm/bmakelib"
   url "https://github.com/bahmanm/bmakelib/releases/download/v0.4.1/bmakelib-0.4.1.tar.gz"
   sha256 "d60a6b0674941bee801571677fa3d6e09f9d85835ec9f790b5f040feed0eada6"
   license "Apache-2.0"
 
+  depends_on "bash" => [:build, :test]
   depends_on "make" => [:build, :test]
   depends_on "perl" => [:build, :test]
-  depends_on "bash" => [:build, :test]
 
+  depends_on "bash"
   depends_on "make"
   depends_on "perl"
-  depends_on "bash"
 
   def install
     system "make", "PREFIX=#{prefix}", "install"
   end
 
   test do
-    mktemp('bmakelib') {
+    mktemp("bmakelib")
+    begin
       File.write(
-        'Makefile',
+        "Makefile",
         ["include bmakelib/bmakelib.mk",
          "PHONY:echo-version",
          "echo-version:",
          "\t@echo $(bmakelib.VERSION)"].join("\n"))
-      assert_equal '0.4.1', shell_output('make -I ${HOMEBREW_PREFIX}/include echo-version').strip
-    }
+      assert_equal "0.4.1", shell_output("make -I ${HOMEBREW_PREFIX}/include echo-version").strip
+    end
   end
 end
