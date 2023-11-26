4bccd1133d6d38255a1a284f9a1b9422eb299626
Bahman Movaqar <bahman@bahmanm.com>
2023-11-26 04:18:31 -0800

    bmakelib v0.6.0

--- bmakelib.rb
+++ bmakelib.rb
@@ -3,8 +3,8 @@
 class Bmakelib < Formula
   desc "It is a minimalist standard library for writing Makefiles"
   homepage "https://github.com/bahmanm/bmakelib"
-  url "https://github.com/bahmanm/bmakelib/releases/download/v0.4.6/bmakelib-0.4.6.tar.gz"
-  sha256 "618166d00125ad78440e0242a4e897b217045c4023e1053f403e568847c23666"
+  url "https://github.com/bahmanm/bmakelib/releases/download/v0.6.0/bmakelib-0.6.0.tar.gz"
+  sha256 "ca29e4609ed77645a4553ddb94a9709990351b2593fbce0bb48901f1d60d6a4e"
   license "Apache-2.0"
 
   depends_on "bash" => [:build, :test]
@@ -33,7 +33,7 @@ class Bmakelib < Formula
         ].join("\n"),
       )
       ENV["PATH"] = "#{ENV["HOMEBREW_PREFIX"]}/opt/make/libexec/gnubin:#{ENV["PATH"]}"
-      assert_equal "0.4.6", shell_output("make -I #{include} echo-version").strip
+      assert_equal "0.6.0", shell_output("make -I #{include} echo-version").strip
     end
   end
 end
